7e812b9beaef580037e37ef71d7268870e7c5c24
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-03-30 22:53:33 +0700

    Clean spaces

--- src/main/java/com/ryoma/restcruddemo/RestcruddemoApplication.java
+++ src/main/java/com/ryoma/restcruddemo/RestcruddemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestcruddemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RestcruddemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RestcruddemoApplication.class, args);
+    }
 }
