ec385fd131c6692388ea25a94cae3b4953db34c3
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-03-30 22:53:05 +0700

    Modify APIs and error handling

--- src/main/java/com/ryoma/restcruddemo/controller/EmployeeRestController.java
+++ src/main/java/com/ryoma/restcruddemo/controller/EmployeeRestController.java
@@ -11,72 +11,30 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
+
     @Autowired
     public EmployeeRestController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
-    public List<Employee> findAll() {
-        return employeeService.findAll();
-    }
-
     @GetMapping("/employees/{id}")
     public Employee findById(@PathVariable int id) {
-        Employee employee = employeeService.findById(id);
-        if (employee == null) {
-            throw new RuntimeException("id not found - " + id);
-        }
-        return employee;
+        return employeeService.findById(id);
     }
 
     @GetMapping("/employees")
-    public Employee findIdByInfo(@RequestBody Employee employee) {
-        int id = employeeService.findIdByInfo(employee);
-        // TODO:
-        if (employee == null) {
-            throw new RuntimeException("id not found - " + id);
-        }
-        return employee;
-    }
-
-    @GetMapping("/employees")
-    public List<Employee> findByFirstName(@RequestParam String firstName) {
-        List<Employee> list = employeeService.findByFirstName(firstName);
-        // TODO:
-        return null;
-    }
-
-    @GetMapping("/employees")
-    public List<Employee> findByLastName(@RequestParam String lastName) {
-        List<Employee> list = employeeService.findByLastName(lastName);
-        // TODO:
-        return null;
-    }
-
-    @GetMapping("/employees")
-    public List<Employee> findByEmail(@RequestParam String email) {
-        List<Employee> list = employeeService.findByEmail(email);
-        // TODO:
-        return null;
+    public List<Employee> findEmployees(@RequestBody(required = false) Employee employee) {
+        return employeeService.findEmployees(employee);
     }
 
     @PostMapping("/employees")
-    public int createEmployee(@RequestBody Employee employee) {
-//        employee.setId(0);
-// id of the deserialized employee is always 0 if the JSON request body does not have "id" property.
-
-        return employeeService.addEmployee(employee);
-    }
-
-    @PutMapping("/employees")
-    public String updateEmployee(@RequestBody Employee employee) {
+    public String createEmployee(@RequestBody Employee employee) {
+//        employee.setId(0); // id is an int instance variable, so its default is 0
         try {
-            employeeService.updateEmployee(employee);
-            return "Update was successful";
+            return String.valueOf(employeeService.addEmployee(employee));
         } catch (Exception e) {
-            String failMessage = "Update failed with error: " + e.getMessage();
+            String failMessage = "Failed with error: " + e.getMessage();
             if (e.getCause() != null) {
                 failMessage += "\ncause: " + e.getCause();
             }
@@ -90,13 +48,33 @@ public class EmployeeRestController {
         }
     }
 
-    @DeleteMapping("/employees/{id}")
-    public String deleteEmployee(@PathVariable int id) {
+    @PutMapping("/employees")
+    public String updateEmployee(@RequestBody Employee employee) {
+        try {
+            employeeService.updateEmployee(employee);
+            return "Success";
+        } catch (Exception e) {
+            String failMessage = "Failed with error: " + e.getMessage();
+            if (e.getCause() != null) {
+                failMessage += "\ncause: " + e.getCause();
+            }
+            if (e.getStackTrace() != null && e.getStackTrace().length != 0) {
+                failMessage += "\nstack trace: ";
+                for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+                    failMessage += "\n" + stackTraceElement.toString();
+                }
+            }
+            return failMessage;
+        }
+    }
+
+    @DeleteMapping("/employees")
+    public String deleteEmployee(@RequestParam int id) {
         try {
             employeeService.deleteById(id);
-            return "Deletion was successful";
+            return "Success";
         } catch (Exception e) {
-            String failMessage = "Deletion failed with error: " + e.getMessage();
+            String failMessage = "Failed with error: " + e.getMessage();
             if (e.getCause() != null) {
                 failMessage += "\ncause: " + e.getCause();
             }
